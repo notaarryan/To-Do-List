b2200871cc4657946e32288008eb12bd4c92ed83
Aryan Parmar <notaarryan@users.noreply.github.com>
2025-07-16 18:35:08 -0400

    Implemented layout and style of the application
    - Completed the layout of the website
    - Also completed the styling of the website

--- webpack.common.js
+++ webpack.common.js
@@ -15,4 +15,20 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
